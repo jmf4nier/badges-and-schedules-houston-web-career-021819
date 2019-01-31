36fd36b06f337051ea3403a1ab563e08fefaa017
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 21:48:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 
 def assign_rooms(name)
   name.collect.with_index do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{index}!"
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
 end
 assign_rooms(names)
