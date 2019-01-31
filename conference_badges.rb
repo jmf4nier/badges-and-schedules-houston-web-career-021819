73c1ca20e97e1ca2194adb65d2164948e69dd3b0
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 21:50:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,14 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 
 def badge_maker(name)
-  puts "Hello, my name is #{name.capitalize}."
+  "Hello, my name is #{name.capitalize}."
 end
-#badge_maker("jason")
+badge_maker("jason")
 
 def batch_badge_creater(name)
   name.collect {|name|  "Hello my name is #{name}."}
 end
-#batch_badge_creater(names)
+batch_badge_creater(names)
 
 def assign_rooms(name)
   name.collect.with_index(1) do |name, index|
