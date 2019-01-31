0cd8d3be1c8ea4332335fe38304a7c386d2387c5
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 21:36:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+
+def badge_maker(name)
+  "Hello, my name is #{name.capitalize}."
+end
+badge_maker("Jason")
+
+def batch_badge_creater
+
+end
+
+def assign_rooms
+  
+end
